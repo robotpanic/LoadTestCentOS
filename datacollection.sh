f6ee76f9feb528775126c909c5fb8eeb6558b1b2
Kalin Iliev <robotpanic@users.noreply.github.com>
2020-03-01 18:53:34 +0200

    Create datacollection.sh

---
+++ datacollection.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+score5=0
+for i in cpu_one_minute mem_swap_one_minute io_one_minute system-resources.data error.data; do
+  ls -l /developers |grep $i
+  if [[ "$?" == 0 ]]; then
+     score5=$((score5+4))
+  fi
+done
+echo $score5
